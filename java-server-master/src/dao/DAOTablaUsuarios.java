96917f21aa1fef30cfd850b2976771cb79c05559
Juan Felipe Ramos Correa <jf.ramos@uniandes.edu.co>
2017-10-04 11:28:52 -0500

    Usuario - VO Y DAO

---
+++ java-server-master/src/dao/DAOTablaUsuarios.java
@@ -0,0 +1,113 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import vos.Usuario;
+
+public class DAOTablaUsuarios {
+	
+	private ArrayList<Object> recursos;
+    private Connection conn;
+    
+    public DAOTablaUsuarios() {
+        recursos = new ArrayList<Object>();
+    }
+    
+    public void cerrarRecursos() {
+        for(Object ob : recursos){
+            if(ob instanceof PreparedStatement)
+                try {
+                    ((PreparedStatement) ob).close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+        }
+    }
+
+    public void setConn(Connection con){
+        this.conn = con;
+    }
+
+    public ArrayList<Usuario> darUsuarios() throws SQLException, Exception {
+        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+
+        String sql = "SELECT * FROM USUARIOS";
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        ResultSet rs = prepStmt.executeQuery();
+
+        while (rs.next()) {
+            insertarUsuario(rs,usuarios);
+        }
+        return usuarios;
+    }
+
+    public ArrayList<Usuario> buscarUsuariosPorId(Long id) throws SQLException, Exception {
+        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+
+        String sql = "SELECT * FROM USUARIOS WHERE ID ='" + id + "'";
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        ResultSet rs = prepStmt.executeQuery();
+
+        while (rs.next()) {
+            insertarUsuario(rs,usuarios);
+        }
+
+        return usuarios;
+    }
+
+    public void updateZona(Usuario usuario) throws SQLException, Exception {
+
+        String sql = "UPDATE USUARIOS SET ";
+        sql += "NOMBRES="+"'"+usuario.getNombres()+"',";
+        sql += "APELLIDOS="+"'"+usuario.getApellidos()+"',";
+        sql += "TIPOID="+"'"+usuario.getTipoId()+"',";
+        sql += "NUMID="+"'"+usuario.getNumId()+"' ";
+        sql += "WHERE ID="+"'"+usuario.getId()+"';";
+        
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        prepStmt.executeQuery();
+    }
+
+    public void addUsuario(Usuario usuario) throws SQLException, Exception {
+
+        String sql = "INSERT INTO USUARIOS (ID, NOMBRES, APELLIDOS, TIPOID, NUMID) VALUES (";
+        sql += "'"+usuario.getId() + "',";
+        sql += "'"+usuario.getNombres()+"',";
+        sql += "'"+usuario.getApellidos()+"',";
+        sql += "'"+usuario.getTipoId()+"',";
+        sql += "'"+usuario.getNumId()+"');";
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        prepStmt.executeQuery();
+
+    }
+
+    public void deleteZona(Usuario usuario) throws SQLException, Exception {
+
+        String sql = "DELETE FROM USUARIOS";
+        sql += " WHERE ID = " + usuario.getId();
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        prepStmt.executeQuery();
+    }
+
+    private void insertarUsuario(ResultSet rs,ArrayList<Usuario> usuarios) throws SQLException{
+    	Long id=rs.getLong("ID");
+        String nombres = rs.getString("NOMBRES");
+        String apellidos = rs.getString("APELLIDOS");
+        String tipo = rs.getString("TIPOID");
+        Long numId = rs.getLong("NUMID");
+        usuarios.add(new Usuario(id,nombres,apellidos,tipo,numId));
+    }
+}
---
+++ java-server-master/src/vos/Usuario.java
@@ -0,0 +1,149 @@
+package vos;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class Usuario {
+	
+////Atributos
+
+	/**
+	 * Id del usuario
+	 */
+	@JsonProperty(value="id")
+	private Long id;
+
+	/**
+	 * Nombres del usuario
+	 */
+	@JsonProperty(value="nombres")
+	private String nombres;
+
+	/**
+	 * Apellidos del usuario
+	 */
+	@JsonProperty(value="apellidos")
+	private String apellidos;
+	
+	/**
+	 * tipo de identificacion del usuario
+	 */
+	@JsonProperty(value="tipoId")
+	private String tipoId;
+	
+	
+	/**
+	 * Numero de identificacion del usuario
+	 */
+	@JsonProperty(value="numId")
+	private Long numId;
+	
+	/**
+	 * Metodo getter del atributo duration
+	 * @return duracion del video en minutos
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * Metodo setter del atributo duration <b>post: </b> La duracion del video
+	 * ha sido cambiado con el valor que entra como parametro
+	 * @param duration - Duracion en minutos del video.
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	/**
+	 * Metodo getter del atributo duration
+	 * @return duracion del video en minutos
+	 */
+	public String getNombres() {
+		return nombres;
+	}
+
+	/**
+	 * Metodo setter del atributo duration <b>post: </b> La duracion del video
+	 * ha sido cambiado con el valor que entra como parametro
+	 * @param duration - Duracion en minutos del video.
+	 */
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	/**
+	 * Metodo getter del atributo duration
+	 * @return duracion del video en minutos
+	 */
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	/**
+	 * Metodo setter del atributo duration <b>post: </b> La duracion del video
+	 * ha sido cambiado con el valor que entra como parametro
+	 * @param duration - Duracion en minutos del video.
+	 */
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	/**
+	 * Metodo getter del atributo duration
+	 * @return duracion del video en minutos
+	 */
+	public String getTipoId() {
+		return tipoId;
+	}
+	
+	
+	/**
+	 * Metodo setter del atributo duration <b>post: </b> La duracion del video
+	 * ha sido cambiado con el valor que entra como parametro
+	 * @param duration - Duracion en minutos del video.
+	 */
+	public void setTipoId(String tipoId) {
+		this.tipoId = tipoId;
+	}
+
+	/**
+	 * Metodo getter del atributo duration
+	 * @return duracion del video en minutos
+	 */
+	public Long getNumId() {
+		return numId;
+	}
+	
+	
+	/**
+	 * Metodo setter del atributo duration <b>post: </b> La duracion del video
+	 * ha sido cambiado con el valor que entra como parametro
+	 * @param duration - Duracion en minutos del video.
+	 */
+	public void setNumId(Long numId) {
+		this.numId = numId;
+	}
+	
+////Constructor
+
+	/**
+	 * Metodo constructor de la clase usuario
+	 * <b>post: </b> Crea el usuario con los valores que entran como parametro
+	 * @param id - Id del usuario.
+	 * @param nombres - Nombres del usuario. name != null
+	 * @param apellidos - Apellidos del usuario. apellido != null
+	 * @param tipoId - Tipo de identificacion del usuario. tipoId != null
+	 * @param numId - Numero de identificacion del usuario. numId != null
+	 * 
+	 */
+	public Usuario (@JsonProperty(value="id")Long id, @JsonProperty(value="nombres")String name,@JsonProperty(value="apellidos")String apellidos, @JsonProperty(value="tipoId")String tipoId, @JsonProperty(value="numId")Long numId) {
+		super();
+		this.id = id;
+		this.nombres = name;
+		this.apellidos = apellidos;
+		this.tipoId= tipoId;
+		this.numId= numId;
+	}
+	
+}
