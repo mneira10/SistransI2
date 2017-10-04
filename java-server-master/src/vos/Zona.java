d572c6e7dcd5db136c8b1bb822f40a963be082d0
Mauricio Neira <m.neira10@uniandes.edu.co>
2017-10-04 10:45:47 -0500

    DAO y VOS de Zona

---
+++ java-server-master/src/vos/Zona.java
@@ -0,0 +1,70 @@
+package vos;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.Date;
+
+public class Zona {
+    @JsonProperty(value="nombre")
+    private String nombre;
+
+    @JsonProperty(value="cerrado")
+    private Boolean cerrado;
+
+    @JsonProperty(value="tipo")
+    private String tipo;
+
+    @JsonProperty(value="aptoDescap")
+    private Boolean aptoDescap;
+
+    @JsonProperty(value="capacidad")
+    private Integer capacidad;
+
+    public Zona(String nombre, Boolean cerrado, String tipo, Boolean aptoDescap, Integer capacidad) {
+        this.nombre = nombre;
+        this.cerrado = cerrado;
+        this.tipo = tipo;
+        this.aptoDescap = aptoDescap;
+        this.capacidad = capacidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getCerrado() {
+        return cerrado;
+    }
+
+    public void setCerrado(Boolean cerrado) {
+        this.cerrado = cerrado;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Boolean getAptoDescap() {
+        return aptoDescap;
+    }
+
+    public void setAptoDescap(Boolean aptoDescap) {
+        this.aptoDescap = aptoDescap;
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+}
