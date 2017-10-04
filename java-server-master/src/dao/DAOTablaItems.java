d2d9d90b29c1d1dc704c3f9ffb224577be8f67e2
Mauricio Neira <m.neira10@uniandes.edu.co>
2017-10-04 12:32:08 -0500

    DAOS y VOS de items

---
+++ java-server-master/src/dao/DAOTablaItems.java
@@ -0,0 +1,122 @@
+package dao;
+
+import vos.Item;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DAOTablaItems {
+    private ArrayList<Object> recursos;
+    private Connection conn;
+    public DAOTablaItems() {
+        recursos = new ArrayList<Object>();
+    }
+    public void cerrarRecursos() {
+        for(Object ob : recursos){
+            if(ob instanceof PreparedStatement)
+                try {
+                    ((PreparedStatement) ob).close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+        }
+    }
+
+    public void setConn(Connection con){
+        this.conn = con;
+    }
+
+    public ArrayList<Item> darItems() throws SQLException, Exception {
+        ArrayList<Item> items = new ArrayList<Item>();
+
+        String sql = "SELECT * FROM ITEMS";
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        ResultSet rs = prepStmt.executeQuery();
+
+        while (rs.next()) {
+            insertarItem(rs,items);
+        }
+        return items;
+    }
+
+    public ArrayList<Item> buscarItemsPorID(Long id) throws SQLException, Exception {
+        ArrayList<Item> items = new ArrayList<Item>();
+
+        String sql = "SELECT * FROM ITEMS WHERE ID =" + id ;
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        ResultSet rs = prepStmt.executeQuery();
+
+        while (rs.next()) {
+            insertarItem(rs,items);
+        }
+
+        return items;
+    }
+
+    public void updateItem(Item item) throws SQLException, Exception {
+
+        String sql = "UPDATE ITEMS SET ";
+        sql += "PERSONALIZACION='" + item.getPersonalizacion() + "',";
+        sql += "ID_PRODUCTO='" + item.getProductoId()+"',";
+        sql += "ID_CARRITO= '" + item.getCarritoId()+"'";
+        sql += "WHERE ID = "+ item.getId();
+
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        prepStmt.executeQuery();
+    }
+
+    public void addItem(Item item) throws SQLException, Exception {
+
+        String sql = "INSERT INTO ITEMS (ID, PERSONALIZACION, ID_PRODUCTO, ID_CARRITO) VALUES (";
+        sql += "'"+item.getId() + "',";
+        sql += "'"+item.getPersonalizacion() + "',";
+        sql += "'"+item.getProductoId()+"',";
+        sql += "'"+item.getCarritoId()+"')";
+
+
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        prepStmt.executeQuery();
+
+    }
+
+    public void deleteItem(Item item) throws SQLException, Exception {
+
+        String sql = "DELETE FROM ITEMS";
+        sql += " WHERE ID = " + item.getId();
+
+        PreparedStatement prepStmt = conn.prepareStatement(sql);
+        recursos.add(prepStmt);
+        prepStmt.executeQuery();
+    }
+
+    private boolean parseBoolean(int num){
+        if(num==0) return false;
+        else return true;
+    }
+
+    private void insertarItem(ResultSet rs,ArrayList<Item> items) throws SQLException{
+        Long id = rs.getLong("ID");
+        String personalizacion= rs.getString("PERSONALIZACION");
+        Long idProd= rs.getLong("ID_PRODUCTO");
+        Long idCarrito = rs.getLong("ID_CARRITO");
+        items.add(new Item(id,personalizacion,idProd,idCarrito));
+    }
+
+    private int antiParseBoolean(Boolean bol){
+        if (bol ) return 1;
+        else return 0;
+    }
+
+
+}
---
+++ java-server-master/src/vos/Item.java
@@ -0,0 +1,54 @@
+package vos;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class Item {
+    @JsonProperty(value="id")
+    private Long id;
+    @JsonProperty(value="personalizacion")
+    private String personalizacion;
+    @JsonProperty(value="productoId")
+    private Long productoId;
+    @JsonProperty(value="carritoId")
+    private Long carritoId;
+
+
+    public Item(Long id, String personalizacion, Long productoId, Long carritoId) {
+        this.id = id;
+        this.personalizacion = personalizacion;
+        this.productoId = productoId;
+        this.carritoId = carritoId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPersonalizacion() {
+        return personalizacion;
+    }
+
+    public void setPersonalizacion(String personalizacion) {
+        this.personalizacion = personalizacion;
+    }
+
+    public Long getProductoId() {
+        return productoId;
+    }
+
+    public void setProductoId(Long productoId) {
+        this.productoId = productoId;
+    }
+
+    public Long getCarritoId() {
+        return carritoId;
+    }
+
+    public void setCarritoId(Long carritoId) {
+        this.carritoId = carritoId;
+    }
+}
